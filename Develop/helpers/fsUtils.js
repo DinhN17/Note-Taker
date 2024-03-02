ef466ca3395be561cf5b8ea40137de8a0497054a
Dinhn17 <nquydinh@yahoo.com>
2024-03-02 21:53:46 +1030

    added post /api/notes route for save note feature

--- Develop/helpers/fsUtils.js
+++ Develop/helpers/fsUtils.js
@@ -4,5 +4,23 @@ const util = require('util');
 // readFile
 const readFromFile = util.promisify(fs.readFile);
 
+// writeToFile
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
 
-module.exports = { readFromFile};
+// readAndAppend
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedData = JSON.parse(data);
+        parsedData.push(content);
+        writeToFile(file, parsedData);
+      }
+    });
+  };
+
+module.exports = { readFromFile, readAndAppend};
--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,12 +1,31 @@
 const notes = require('express').Router();
 
 // helper util for read and write files
-const {readFromFile} = require('../helpers/fsUtils');
+const {readFromFile, readAndAppend} = require('../helpers/fsUtils');
 
 // GET Route for retrieving all notes
 notes.get('/', (req, res) => {
     readFromFile('./db/db.json')
     .then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for save note
+notes.post('/', (req, res) => {
+    console.log(req.body);
+
+    const {title, text} = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text
+        }
+
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note ${newNote.title} saved successfully`);
+    } else {
+        res.errored(`Error in saving note ${newNote.title}`);
+    }
 });
 
 module.exports = notes;
