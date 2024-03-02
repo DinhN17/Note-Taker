4a9c1a1af74a4c13e30df2c0731644cd10ca1fac
Dinhn17 <nquydinh@yahoo.com>
2024-03-03 00:09:07 +1030

    added delete /api/:id route for delete note feature

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,7 +1,7 @@
 const notes = require('express').Router();
 
 // helper util for read and write files
-const {readFromFile, readAndAppend} = require('../helpers/fsUtils');
+const {readFromFile, readAndAppend, writeToFile} = require('../helpers/fsUtils');
 
 // generate unique id
 const {v4: uuidv4} = require('uuid');
@@ -31,5 +31,32 @@ notes.post('/', (req, res) => {
         res.errored(`Error in saving note ${newNote.title}`);
     }
 });
+
+// DELETE Route for delete note
+notes.delete('/:id', (req, res) => {
+    console.log(req.params.id);
+
+    if (req.params.id) {
+        const requestedId = req.params.id;
+
+        readFromFile('./db/db.json')
+        .then((data) => {
+        let notes = JSON.parse(data);
+
+        for (let index = 0; index < notes.length; index++) {
+            const element = notes[index];
+            if (element.id === requestedId) {
+                notes.splice(index, 1);
+                break;
+            };
+        };
+
+        writeToFile('./db/db.json', notes);
+        res.json(`Note Id ${requestedId} deleted successfully`);
+    });
+    } else {
+        res.errored(`Error in deleting note id ${requestedId}`);
+    };
+})
 
 module.exports = notes;
