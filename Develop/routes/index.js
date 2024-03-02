2de946cb8d4916020bcca4c884c48f5c2d5b0a63
Dinhn17 <nquydinh@yahoo.com>
2024-03-03 01:10:19 +1030

    fixing port for heroku

--- Develop/routes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-// Import modular router for /notes
-const notesRouter = require('./notes');
-
-router.use('/notes', notesRouter);
-
-module.exports = router;
