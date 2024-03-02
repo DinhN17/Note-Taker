e180fd0692b0cfab4124edfe79701ded02dc86aa
Dinhn17 <nquydinh@yahoo.com>
2024-03-02 21:03:46 +1030

    added get /api/notes route

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,4 +1,12 @@
 const notes = require('express').Router();
 
+// helper util for read and write files
+const {readFromFile} = require('../helpers/fsUtils');
+
+// GET Route for retrieving all notes
+notes.get('/', (req, res) => {
+    readFromFile('./db/db.json')
+    .then((data) => res.json(JSON.parse(data)));
+});
 
 module.exports = notes;
